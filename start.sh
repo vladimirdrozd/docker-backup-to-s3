562a5305ea8f1120a209c14bfc4f0c685e351f70
vladimirdrozd <vladimir.drozd@gmail.com>
2017-08-03 10:47:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
 
-set -e
-
-: ${ACCESS_KEY:?"ACCESS_KEY env variable is required"}
-: ${SECRET_KEY:?"SECRET_KEY env variable is required"}
-: ${S3_PATH:?"S3_PATH env variable is required"}
-export DATA_PATH=${DATA_PATH:-/data/}
+ACCESS_KEY=${ACCESS_KEY:?"ACCESS_KEY env variable is required"}
+SECRET_KEY=${SECRET_KEY:?"SECRET_KEY env variable is required"}
+S3_PATH=${S3_PATH:?"S3_PATH env variable is required"}
+DATA_PATH=${DATA_PATH:-/data/}
 CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
 
 echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
 echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
 
 if [[ "$1" == 'no-cron' ]]; then
-    exec /sync.sh
-elif [[ "$1" == 'delete' ]]; then
-    exec /usr/local/bin/s3cmd del -r "$S3_PATH"
+    exec s3cmd sync --verbose "$DATA_PATH" "$S3_PATH"
+elif [[ "$1" == 'restore' ]]; then
+    exec s3cmd sync --verbose "$S3_PATH" "$DATA_PATH"
 else
-    LOGFIFO='/var/log/cron.fifo'
-    if [[ ! -e "$LOGFIFO" ]]; then
-        mkfifo "$LOGFIFO"
-    fi
-    CRON_ENV="PARAMS='$PARAMS'"
-    CRON_ENV="$CRON_ENV\nDATA_PATH='$DATA_PATH'"
-    CRON_ENV="$CRON_ENV\nS3_PATH='$S3_PATH'"
-    echo -e "$CRON_ENV\n$CRON_SCHEDULE /sync.sh > $LOGFIFO 2>&1" | crontab -
-    crontab -l
-    cron
-    tail -f "$LOGFIFO"
+    echo "$CRON_SCHEDULE s3cmd sync \"$DATA_PATH\" \"$S3_PATH\"" | crontab -
+    exec cron -f
 fi
